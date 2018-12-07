24325329656d864bdd905d2c2c7b26fe8512567a
unknown <DoAnhTuan>
2018-12-07 22:09:18 +0700

    initial commit

--- session5/homework/bai3.py
+++ session5/homework/bai3.py
@@ -2,7 +2,7 @@ result = [35,36,40,44]
 print("Answer the following algebra question:")
 
 print("If x = 8, then what is the value of 4(x + 3) ?")
-a= 4*(8+3)
+
 
 for index, item in enumerate(result):
     print(index +1, item,sep = "." )
